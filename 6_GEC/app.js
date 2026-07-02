0afd96a08129cd5741e05346d97e9e983b8ff109
rlavinsh <rlavinsh@gmail.com>
2026-07-03 01:18:14 +0530

    Execution context completed

--- 6_GEC/app.js
+++ 6_GEC/app.js
@@ -1,5 +1,50 @@
-var a = 10;
-var b = 20;
+// var a = 10;
+// var b = 20;
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
+
+// Example-2
+
+// console.log("code start");
+// var age = 20;
+// function greet() {
+//   var phone = 1234;
+//   var city = "bhopal";
+//   console.log("Inside Greet Function");
+// }
+
+// greet();
+// console.log(age);
+// console.log("code ends");
+
+// Example -3
+
+// console.log("code start");
+// var num1 = 20;
+// var num2 = 40;
+
+// function c() {
+//   var d = 50;
+//   function e(para) {
+//     console.log(para);
+//   }
+//   console.log("Inside Inner function");
+//   e("John");
+//   console.log(d);
+//   return "Javascript";
+// }
+
+// var f = c();
+// console.log(f);
+// console.log("code ends");
+
+// console.log(
+//   "========================================================================",
+// );
+// console.log(age);
+// const age = 20;
+// sayHello();
+// let sayHello = function () {
+//   console.log("hello");
+// };
