14c3762ae3e92791925685151bae7368db365475
rlavinsh <rlavinsh@gmail.com>
2026-07-04 19:57:49 +0530

    till hoisting completed

---
+++ 7_hoisting/app.js
@@ -0,0 +1,12 @@
+// console.log(a);
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// let a = 10;
+// console.log(a);
+
+// console.log(a);
+greet();
+const greet = function () {
+  console.log("hello");
+};
