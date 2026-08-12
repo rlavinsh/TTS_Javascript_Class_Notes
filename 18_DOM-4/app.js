20f78fd10dd99127e12be3d8006555e4c62a79d9
rlavinsh <rlavinsh@gmail.com>
2026-08-12 11:26:47 +0530

    dom

---
+++ 18_DOM-4/app.js
@@ -0,0 +1,49 @@
+// li k parent par background color dena hein
+// lekin apko background color dena hein scooty k refrence se
+
+// let scooty = document.getElementsByClassName("scooty");
+// let scooty = document.querySelector(".scooty");
+// console.log(scooty);
+
+// console.log(scooty.parentElement.style.backgroundColor);
+// scooty.parentElement.parentElement.style.backgroundColor = "orange";
+
+// let vechile = document.querySelector(".vechile");
+// console.log(vechile);
+
+// // vechile.lastElementChild.textContent = "hello";
+// vechile.firstElementChild.style.backgroundColor = "yellow"
+
+// console.log(vechile.children);
+
+// let scooty = document.querySelector(".scooty");
+// console.log(scooty.nextElementSibling.firstElementChild.textContent);
+
+// let btn = (scooty.nextElementSibling.innerText = "duniya");
+// console.log(btn);
+// scooty.previousElementSibling.style.backgroundColor = "red";
+
+// let heading = document.querySelector("h1");
+// console.log(heading);
+
+// console.log(heading.nextElementSibling.firstElementChild.nextElementSibling);
+
+// let remove = document.querySelector(".remove");
+// console.log(remove);
+
+// let btn = document.querySelector("#btn");
+// btn.parentElement.parentElement.style.backgroundColor = "green";
+
+// btn.closest(".vechile").style.backgroundColor = "green";
+// console.log(btn);
+
+// let scooty = document.querySelector(".scooty");
+// console.log(scooty.remove());
+
+// let heading = document.querySelector("h1")
+// let heading = document.getElementsByTagName("h1");
+// console.log(heading[0]);
+
+let heading2 = document.getElementsByClassName("hello");
+// let heading2 = document.querySelector(".hello");
+console.log(heading2[0]);
